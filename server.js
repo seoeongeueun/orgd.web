0de36df3cae9eed7fbfe9a214c7e4cd5443b5712
seongeunp <sep@ba-ton.kr>
2024-09-06 16:10:54 +0900

    feat: 서브 텍스트 노출 소켓 이벤트 연결

--- server.js
+++ server.js
@@ -17,38 +17,59 @@ app.prepare().then(() => {
 		},
 	});
 
-	let userCursors = {}; //유저의 커서를 저장
+	// let userCursors = {}; //유저의 커서를 저장
+	// io.on("connection", (socket) => {
+	// 	const userId = socket.handshake.query.userId;
+	// 	console.log("Client connected:", userId);
+
+	// 	if (Object.keys(userCursors).length !== 0) {
+	// 		const firstUserId = Object.keys(userCursors)[0];
+
+	// 		if (firstUserId !== userId) {
+	// 			socket.emit("is_not_host");
+	// 		}
+	// 	} else {
+	// 	}
+
+	// 	socket.on("open_dialog", () => {
+	// 		io.emit("open_dialog");
+	// 	});
+	// 	console.log("Emitting existing cursors:", userCursors);
+	// 	socket.emit("existing_cursors", userCursors);
+
+	// 	socket.on("cursor_move", (position) => {
+	// 		userCursors[userId] = position;
+	// 		socket.broadcast.emit("cursor_update", {
+	// 			id: userId,
+	// 			position,
+	// 		});
+	// 	});
+
+	// 	socket.on("show_subtext", ({ mainTextId }) => {
+	// 		socket.broadcast.emit("show_subtext", { mainTextId });
+	// 	});
+
+	// 	socket.on("disconnect", () => {
+	// 		console.log("Client disconnected:", socket.id);
+	// 		delete userCursors[userId];
+	// 		socket.broadcast.emit("cursor_remove", { id: userId });
+	// 	});
+
+	// 	});
+	let subTextVisibilityState = {}; // 현재 활성화된 서브텍스트를 저장
+
 	io.on("connection", (socket) => {
-		const userId = socket.handshake.query.userId;
-		console.log("Client connected:", userId);
+		console.log("A user connected:", socket.id);
 
-		if (Object.keys(userCursors).length !== 0) {
-			const firstUserId = Object.keys(userCursors)[0];
+		socket.emit("initial_visibility", subTextVisibilityState);
 
-			if (firstUserId !== userId) {
-				socket.emit("is_not_host");
-			}
-		} else {
-		}
-
-		socket.on("open_dialog", () => {
-			io.emit("open_dialog");
-		});
-		console.log("Emitting existing cursors:", userCursors);
-		socket.emit("existing_cursors", userCursors);
-
-		socket.on("cursor_move", (position) => {
-			userCursors[userId] = position;
-			socket.broadcast.emit("cursor_update", {
-				id: userId,
-				position,
-			});
+		socket.on("show_subtext", ({ mainTextId, subtextVisible }) => {
+			subTextVisibilityState[mainTextId] = subtextVisible;
+			socket.broadcast.emit("show_subtext", { mainTextId, subtextVisible });
 		});
 
 		socket.on("disconnect", () => {
-			console.log("Client disconnected:", socket.id);
-			delete userCursors[userId];
-			socket.broadcast.emit("cursor_remove", { id: userId });
+			console.log("User disconnected:", socket.id);
 		});
 	});
 
