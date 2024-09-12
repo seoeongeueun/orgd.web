0e5a6d23186b12753850e077d41f070e827b7357
seoeongeueun <erin9901@gmail.com>
2024-09-12 21:35:17 +0900

    fix: middleware 오류 수정

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from "jose"; // Import jwtVerify from jose
 
 export async function middleware(req) {
+	console.log(`Incoming request to: ${req.nextUrl.pathname}`);
 	const token = req.cookies.get("authToken");
 
 	if (!token) {
@@ -10,12 +11,10 @@ export async function middleware(req) {
 	}
 
 	try {
-		const { payload } = await jwtVerify(
+		await jwtVerify(
 			token.value,
 			new TextEncoder().encode(process.env.JWT_SECRET)
 		);
-		const requestHeaders = new Headers(req.headers);
-		requestHeaders.set("Authorization", `Bearer ${token.value}`);
 
 		const response = NextResponse.next();
 		response.headers.set("Authorization", `Bearer ${token.value}`);
@@ -27,5 +26,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-	matcher: ["/settings", "/api/:path*"],
+	matcher: ["/settings/:path*", "/api/:path*"],
 };
