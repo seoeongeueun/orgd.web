90a8f9b80b64a8abec532092e8ca24472ed42b6f
seongeunp <sep@ba-ton.kr>
2024-09-25 14:32:36 +0900

    style: 대기 화면 기본 구조 추가

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
+"use client";
+import { useEffect, useState } from "react";
 import SharedPage from "./components/SharedPage";
 
 export default function Page() {
+	const [message, setMessage] = useState("텍스트를 눌러보세요");
+	const [showLoading, setShowLoading] = useState(true);
+
 	return (
-		<div id="scroll-div" className="overflow-auto">
-			<SharedPage isEditMode={false} />
-		</div>
+		<>
+			<div id="scroll-div" className="overflow-auto">
+				<SharedPage setMessage={setMessage} setShowLoading={setShowLoading} />
+			</div>
+			{/* {showLoading && <LoadingScreen message={message} />} */}
+		</>
 	);
 }
