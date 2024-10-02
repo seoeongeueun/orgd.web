af5aa2d96d1b6cecd4ac1989058fb347b91e77eb
seongeunp <sep@ba-ton.kr>
2024-10-02 21:52:56 +0900

    feat: 설정 화면에서 초기화 트리거시 랜덤 uids도 전달

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
 import SharedPage from "./components/SharedPage";
 
 export default function Page() {
-	// return (
-	// 	<div id="scroll-div" className="overflow-auto h-full">
-	// 		<SharedPage isEditMode={false} />
-	// 	</div>
-	// );
 	return <SharedPage isEditMode={false} />;
 }
